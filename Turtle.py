2a7fd4e68ff092754b197328b87cf1f25be81ec4
fBencef <36234890+fBencef@users.noreply.github.com>
2019-03-06 08:30:36 +0100

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -2,9 +2,8 @@ import turtle
 ablak = turtle.Screen()
 alex = turtle.Turtle
 n = 3
-alex.forward(150)
-alex.left(360 / n)
-alex.forward(150)
-alex.left(360 / n)
-alex.forward(150)
-alex.left(360 / n)
+
+#for jucika in cipőbolt(cipők_száma)
+for i in range(n):
+  alex.forward(150)
+  alex.left(360 / n)
